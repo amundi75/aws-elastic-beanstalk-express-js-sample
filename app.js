f9b526ae6d13cfa8fa132bb15f23839c0f592469
amundi75 <angele.mundi@gmail.com>
2021-08-20 13:21:22 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-
-app.get('/', (req, res) => res.send('I created my first delivery pipeline. Isn't that awesome?!'));
+app.get('/', (req, res) => res.send('I created my first delivery pipeline. That is awesome!'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
